1a29df7e2696acd8c4da0c56cedbb96ce06e0970
Isabel <isabelcorreabernal@gmail.com>
2024-04-17 20:34:08 -0600

    Este es el codigo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace itG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu Mensaje");
+            string Msj = Console.ReadLine();
+            //string Msj = "Hola";
+            Char[] Txt = Msj.ToCharArray();
+
+            for (int i = Txt.Length-1; i>=0; i--)
+            {
+                Console.Write(Txt[i]);         
+            }
+
+
+        }
+    }
+}
